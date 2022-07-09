86698d0a05864a1e849a4698a3478a078a2b38d6
alaverArt <alaverArt@yandex.ru>
2022-07-09 15:44:23 +0300

    Сделал хэдер

--- frontend/cashboomerang/src/layouts/MainLayout.vue
+++ frontend/cashboomerang/src/layouts/MainLayout.vue
@@ -1,6 +1,21 @@
 <template>
   <q-layout>
+    <div>
+      <q-toolbar class="q-pt-sm q-pb-xl row items-start q-px-md bg-grey-14 text-white">
+        <q-avatar size="4.5rem">
+          <img src="../assets/header/logo.svg">
+        </q-avatar>
 
+        <q-toolbar-title class="xs-hide">CashBoomerang</q-toolbar-title>
+        <q-space></q-space>
+        <div class="column">
+          <q-btn style="margin-bottom: 0.1rem;" class="q-pa-1" flat dense>
+            <img style="width:2.5rem" src="../assets/header/userLogo.svg"/>
+          </q-btn>
+          <q-badge color="red">ID: {{userId}}</q-badge>
+        </div>
+      </q-toolbar>
+    </div>
   </q-layout>
 </template>
 
@@ -9,14 +24,13 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
-
   components: {
 
   },
 
   data(){
     return{
-
+      userId:'5745'
     }
   }
 })
