d9a8d0124b3e251390d4bce4693ef7e681a84247
alaverArt <alaverArt@yandex.ru>
2022-07-09 10:04:13 +0300

    init quasar app

---
+++ frontend/cashboomerang/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  data(){
+    return{
+
+    }
+  }
+})
+</script>
